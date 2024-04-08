29a9f891074a104e724128a38455b20c0e374e4c
shreyba <144711312+shreyba@users.noreply.github.com>
2024-04-08 14:32:24 -0400

    Update Job.h

--- Job.h
+++ Job.h
@@ -1,1 +1,24 @@
-#pragma once
+//
+//  job.hpp
+//  200project2
+//
+//  Created by shreyas babel on 08/04/24.
+//
+
+#ifndef JOB_H
+#define JOB_H
+
+struct Job {
+    char type;
+    int jobTypeNumber;
+    int overallJobNumber;
+    int arrivalTime;
+    int processingTime;
+    int timeInQueue;
+};
+
+#endif
+
+
+
+
