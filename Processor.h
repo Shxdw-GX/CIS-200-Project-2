3ce8f9d897a918c18517277454ddce32393379e4
shreyba <144711312+shreyba@users.noreply.github.com>
2024-04-08 14:33:45 -0400

    Update Processor.h

--- Processor.h
+++ Processor.h
@@ -1,3 +1,40 @@
-#pragma once
-#include "Job.h"
-#include "Queue.h"
+//
+//  processor.hpp
+//  200project2
+//
+//  Created by shreyas babel on 08/04/24.
+//
+
+#ifndef processor_hpp
+#define processor_hpp
+
+#include "job.hpp"
+
+class Processor {
+private:
+    int processorNumber;
+    bool runningStatus;
+    bool isBusy;
+    int idleTime;
+    int runningTime;
+    Job currentJob;
+
+public:
+    Processor(int num);
+
+    bool hasJob() const;
+    bool isHighPriority() const;
+    Job getCurrentJob() const;
+    void prepareForNewJob();
+    void insertJob(const Job& job);
+    int getProcessorNumber() const;
+    int getIdleTime() const;
+    int getRunningTime() const;
+    void reduceProcessingTime();
+    void incrementIdleTime();
+    void setProcessorNumber(int num);
+    bool isRunning() const;
+};
+
+
+#endif /* processor_hpp */
